faf86abcc9b735cad2260b33a6465a2c5a4f2acd
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-07-15 14:40:13 +0200

    Fix compilation with wireshark 4
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

--- packet-minecraft.c
+++ packet-minecraft.c
@@ -1,4 +1,4 @@
-#include <config.h>
+#include <ws_version.h>
 
 #define WS_BUILD_DLL
 
@@ -15,8 +15,8 @@
 #include <stdint.h>
 
 WS_DLL_PUBLIC_DEF const gchar plugin_version[] = "0.0.0";
-WS_DLL_PUBLIC_DEF const int plugin_want_major = VERSION_MAJOR;
-WS_DLL_PUBLIC_DEF const int plugin_want_minor = VERSION_MINOR;
+WS_DLL_PUBLIC_DEF const int plugin_want_major = WIRESHARK_VERSION_MAJOR;
+WS_DLL_PUBLIC_DEF const int plugin_want_minor = WIRESHARK_VERSION_MINOR;
 
 WS_DLL_PUBLIC void plugin_register(void);
 
